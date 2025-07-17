b26c36664e88d5584d3ab60247c027cf90b78613
zhu.yumeng <28043724+leozym@users.noreply.github.com>
2025-07-17 23:31:14 +0800

    init

---
+++ src/main/java/com/ym/yunmeet/YunMeetApplication.java
@@ -0,0 +1,13 @@
+package com.ym.yunmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunMeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YunMeetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ym/yunmeet/YunMeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ym.yunmeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YunMeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
